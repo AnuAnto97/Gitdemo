4b8145e7e2758b0eae1669ca3558e9777b02df82
Anu Anto <84711182+AnuAnto97@users.noreply.github.com>
2022-03-01 14:32:10 +0530

    Added CW "Good Afternoon"

--- PrjGet1000Git/PrjGet1000Git/Program.cs
+++ PrjGet1000Git/PrjGet1000Git/Program.cs
@@ -7,6 +7,7 @@ namespace PrjGet1000Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Afternoon!");
         }
     }
 }
