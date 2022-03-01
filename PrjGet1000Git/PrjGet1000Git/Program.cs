3f8e2476e5eb2b3e68df73fbae5acf8f78691745
AnuAnto97 <anuanto1997@hotmail.com>
2022-03-01 14:29:23 +0530

    console Application Added

---
+++ PrjGet1000Git/PrjGet1000Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrjGet1000Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
